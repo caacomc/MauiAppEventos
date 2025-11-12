b7124ca03294da109e9ad137a625c6ff19b1d4cb
caroline.campos <caacomc@gmail.com>
2025-11-12 20:18:36 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppEventos/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiAppEventos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double CustoPorParticipante = Convert.ToDouble(txt_CustoPorParticipante.Text);
+
+
+            } catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+
+        }
+    }
+}
